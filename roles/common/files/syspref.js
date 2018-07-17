d91bf27d6ba74281d8cf77d92613ee56bbc3ba88
Den <den@vdsinside.com>
2018-07-17 15:38:55 +0300

    - added firefox syspref.js

---
+++ roles/common/files/syspref.js
@@ -0,0 +1,17 @@
+// This file can be used to configure global preferences for Firefox
+// Example: Homepage
+pref("browser.startup.homepage", "https://google.com");
+pref("browser.cache.memory.enable", true);
+pref("browser.cache.disk.enable", false);
+pref("browser.cache.disk.smart_size.enabled", false);
+pref("browser.cache.disk.capacity", 0);
+pref("toolkit.cosmeticAnimations.enabled", false);
+pref("browser.sessionhistory.max_entries", 20);
+pref("browser.ctrlTab.previews", false);
+pref("browser.urlbar.maxRichResults", -1);
+pref("browser.sessionstore.interval", 60000);
+pref("dom.max_script_run_time", 20);
+pref("config.trim_on_minimize", true);
+pref("browser.urlbar.clickSelectsAll", true);
+pref("browser.cache.offline.capacity", 256000);
+pref("browser.download.saveLinkAsFilenameTimeout", 2000);
